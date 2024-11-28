231f8296c9776d02f732db091af948f6e5a52dce
tanishqDevx <tanishqjain000111@gmail.com>
2024-11-28 20:27:44 +0530

    add good css

---
+++ app/buttonn/buttonn.js
@@ -0,0 +1,7 @@
+import { RainbowButton } from "@/components/ui/rainbow-button";
+
+function RainbowButtonDemo() {
+  return <RainbowButton>Let's Sign in</RainbowButton>;
+}
+
+export default RainbowButtonDemo;
